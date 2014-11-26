7df17fec3878917bacef230d892d563ce3941877
piotrowskislawomir <piotrowski.slawomir590@gmail.com>
2014-11-26 22:39:31 +0100

    creating rabbitmq classes

--- Communicator.Queue/Interfaces/IQueueService.cs
+++ Communicator.Queue/Interfaces/IQueueService.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
 
 namespace Communicator.Queue.Interfaces
 {
     public interface IQueueService
     {
+        void Initialize();
+        IBasicConsumer CreateConsumer(string key);
+        void SendData(string key, byte[] data);
     }
 }
--- Communicator.Queue/Services/RabbitMqService.cs
+++ Communicator.Queue/Services/RabbitMqService.cs
@@ -28,20 +28,21 @@ namespace Communicator.Queue.Services
             _model = _queueConnection.CreateModel(ConfigurationApp.Host, ConfigurationApp.UserName, ConfigurationApp.Password);
         }
 
-        public void CreateConsumer(string key)
+        public IBasicConsumer CreateConsumer(string key)
         {
             var queue = _model.QueueDeclare();
             var consumer = new EventingBasicConsumer(_model);
-            consumer.Received +=
-                (_, msg) => Console.WriteLine("Consumer received message {0}", Encoding.UTF8.GetString(msg.Body));
             _model.QueueBind(queue.QueueName, ConfigurationApp.ExchangeName, key);
             _model.BasicConsume(queue.QueueName, false, consumer);
+            return consumer;
         }
 
-        public void SendData(string key)
+        public void SendData(string key, byte[] data)
         {
-            _model.BasicPublish(ConfigurationApp.ExchangeName, key, _model.CreateBasicProperties(), Encoding.UTF32.GetBytes("widomows"));
+            var properties = _model.CreateBasicProperties();
+            properties.SetPersistent(true);
 
+            _model.BasicPublish(ConfigurationApp.ExchangeName, key, properties, data);
         }
 
     }
