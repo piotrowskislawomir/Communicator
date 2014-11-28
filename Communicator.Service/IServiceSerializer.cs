1dd15ddedbb321aa19998f89b7b1d25cb446c410
s384000 <ttrolu@gmail.com>
2014-11-28 10:12:11 +0100

    JSonSerializer
    File: IServiceSerializer, JSonSerializer

---
+++ Communicator.Service/IServiceSerializer.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Communicator.Service
+{
+    interface IServiceSerializer
+    {
+            byte[] Serialize<T>(T dto);
+            T Deserialize<T>(byte[] data);
+    }
+}
---
+++ Communicator.Service/JSonSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace Communicator.Service
+{
+    class JSonSerializer : IServiceSerializer
+    {
+            public byte[] Serialize<T>(T dto)
+            {
+                using (var stream = new MemoryStream())
+                {
+                    var ser = new DataContractJsonSerializer(typeof(T));
+                    ser.WriteObject(stream, dto);
+                    return stream.ToArray();
+                }
+            }
+
+            public T Deserialize<T>(byte[] data)
+            {
+                using (var stream = new MemoryStream(data))
+                {
+                    var ser = new DataContractJsonSerializer(typeof(T));
+                    T dt = (T)ser.ReadObject(stream);
+                    return dt;
+                }
+            }
+    }
+}
