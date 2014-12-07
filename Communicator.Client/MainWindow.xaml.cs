9b28892af73b6eab394c599a50200c5570ab7ab3
modliszka <nikapia@wp.pl>
2014-12-07 20:10:39 +0100

    Client - added app window base

--- Communicator.Client/MainWindow.xaml.cs
+++ Communicator.Client/MainWindow.xaml.cs
@@ -22,16 +22,10 @@ namespace Communicator.Client
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    
-    
-    public partial class MainWindow : Window
-    {
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-                this.DragMove();
-        }
-        
+
+
+    public sealed partial class MainWindow : ApplicationWindowBase
+    {        
         public MainWindow()
         {
             InitializeComponent();
